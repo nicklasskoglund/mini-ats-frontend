43c760a624840d697c45a97dd8a10ba7712c3d2b
nickklasskoglund <nicklas.jobb@gmail.com>
2026-09-16 23:29:23 +0200

    test(e2e): replace scaffold smoke test with a login flow test

--- e2e/app.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test'
-
-test('startsidan visar appnamnet', async ({ page }) => {
-  await page.goto('/')
-  await expect(page.getByRole('heading', { name: 'Mini-ATS' })).toBeVisible()
-})
